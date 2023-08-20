2aa8eb6285c4b13dd895db91ec33b6700c526716
pamudu123 <pamudu123456789@gmail.com>
2023-08-20 21:34:24 +0530

    add new results

--- LLE_UNET_layers_visualization/resources/create_grid_images.py
+++ LLE_UNET_layers_visualization/resources/create_grid_images.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-def create_grid(images_info, rows, cols):
+def create_grid(images_info, rows, cols,save_img_name):
     fig, axes = plt.subplots(rows, cols, figsize=(16, 12))
     plt.subplots_adjust(wspace=0.1, hspace=0.2)
     
@@ -24,29 +24,35 @@ def create_grid(images_info, rows, cols):
                 ax.set_title(title)
     
     
-    plt.savefig('layes.png', dpi=300)
+    plt.savefig(save_img_name, dpi=300)
     plt.show()
 
+<<<<<<< HEAD
 # List of image data (path,title)
+=======
+# List of image data (path, row, col, title)
+folder_name = 'IMG_800'
+
+>>>>>>> 32d75e7 (add new results)
 image_data = [
-    ("layer_outputs/block1_pool.png", "1"),
-    ("layer_outputs/block1_conv2.png","5"),
-    ("layer_outputs/concatenate_4.png","10"),
+    (f"layer_outputs/{folder_name}/block1_pool.png", "1"),
+    (f"layer_outputs/{folder_name}/block1_conv2.png","5"),
+    (f"layer_outputs/{folder_name}/concatenate_4.png","10"),
 
-    ("layer_outputs/block1_pool.png", "2"),
-    ("layer_outputs/block1_conv2.png","6"),
-    ("layer_outputs/concatenate_4.png","11"),
+    (f"layer_outputs/{folder_name}/block1_pool.png", "2"),
+    (f"layer_outputs/{folder_name}/block1_conv2.png","6"),
+    (f"layer_outputs/{folder_name}/concatenate_4.png","11"),
 
-    ("layer_outputs/block1_pool.png", "3"),
-    ("layer_outputs/block1_conv2.png","7"),
-    ("layer_outputs/concatenate_4.png","12"),
+    (f"layer_outputs/{folder_name}/block1_pool.png", "3"),
+    (f"layer_outputs/{folder_name}/block1_conv2.png","7"),
+    (f"layer_outputs/{folder_name}/concatenate_4.png","12"),
 
-    ("layer_outputs/block1_pool.png", "4"),
-    ("layer_outputs/block1_conv2.png","8"),
-    ("layer_outputs/concatenate_4.png","13"),
+    (f"layer_outputs/{folder_name}/block1_pool.png", "4"),
+    (f"layer_outputs/{folder_name}/block1_conv2.png","8"),
+    (f"layer_outputs/{folder_name}/concatenate_4.png","13"),
 
     ("NAN", ""),
-    ("layer_outputs/block1_conv2.png","9"),
+    (f"layer_outputs/{folder_name}/block1_conv2.png","9"),
     ("NAN",""),
 
 ]
@@ -54,4 +60,5 @@ image_data = [
 
 rows = 5
 cols = 3
-create_grid(image_data, rows, cols)
+save_name = f"layer_outputs/{folder_name}/{folder_name}_Layers.png"
+create_grid(image_data, rows, cols,save_name)
